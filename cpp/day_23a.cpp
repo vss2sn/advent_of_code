c987075f35be4bc10c62b1664e602e9926e11412
vss <28676655+vss2sn@users.noreply.github.com>
2021-04-09 14:38:46 -0400

    Refactor day 23a to use algorithm used in 23b

--- cpp/day_23a.cpp
+++ cpp/day_23a.cpp
@@ -1,76 +1,68 @@
 #include <algorithm>
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+
+constexpr int n_cups = 9;
+constexpr int n_rounds = 100;
+
 int main() {
   std::fstream file{"../input/day_23_input"};
   std::string input;
-  std::vector<int> order;
+  std::vector<int> order(n_cups + 1);
+  std::vector<int> input_v;
   std::getline(file, input);
   input.erase(std::remove_if(std::begin(input), std::end(input),
                              [](const char c) { return !isprint(c); }),
               std::end(input));
-  for (const auto& c : input) {
-    order.push_back(c - '0');
+  for (int i = 0; i < input.size(); i++) {
+    input_v.push_back(input[i] - '0');
   }
-  int current_index = 0;
+  for (int i = 0; i < input_v.size() - 1; i++) {
+    order[input_v[i]] = input_v[i + 1];
+  }
+  order[input_v[input_v.size() - 1]] = input_v[0];
+  int cur_val = input_v[0];
   int round = 0;
   int n_pick_up = 3;
-  int low = *std::min_element(std::begin(order), std::end(order));
+  int low = *std::min_element(std::begin(input_v), std::end(input_v));
   int high = *std::max_element(std::begin(order), std::end(order));
-  while (round < 100) {
-    int cur_val = order[current_index];
+  while (round < n_rounds) {
     std::vector<int> cups_picked_up;
-    int delta = 0;
+    int picked_val = order[cur_val];
     for (int i = 0; i < n_pick_up; i++) {
-      if (current_index + 1 < order.size()) {
-        cups_picked_up.push_back(order[current_index + 1]);
-        order.erase(std::begin(order) + current_index + 1);
-      } else {
-        cups_picked_up.push_back(
-            order[current_index + 1 + delta - order.size()]);
-        order.erase(std::begin(order) + current_index + 1 + delta -
-                    order.size());
-        delta--;
-      }
+      cups_picked_up.push_back(picked_val);
+      picked_val = order[picked_val];
     }
+    order[cur_val] = order[cups_picked_up.back()];
     int to_find = cur_val - 1;
-    auto it = std::find(std::begin(order), std::end(order), to_find);
-    while (it == std::end(order)) {
+    if (to_find < low) {
+      to_find = high;
+    }
+    auto it = std::find(std::begin(cups_picked_up), std::end(cups_picked_up),
+                        to_find);
+    while (it != std::end(cups_picked_up)) {
       to_find--;
       if (to_find < low) {
         to_find = high;
       }
-      it = std::find(std::begin(order), std::end(order), to_find);
+      it = std::find(std::begin(cups_picked_up), std::end(cups_picked_up),
+                     to_find);
     }
-    int insert_index = std::distance(std::begin(order), it);
-    for (int i = 0; i < n_pick_up; i++) {
-      if (insert_index + i + 1 < order.size()) {
-        order.insert(std::begin(order) + insert_index + i + 1,
-                     cups_picked_up[i]);
-      } else {
-        order.push_back(cups_picked_up[i]);
-      }
-    }
+    auto temp = order[to_find];
+    order[to_find] = cups_picked_up[0];
+    order[cups_picked_up.back()] = temp;
     round++;
-    current_index =
-        std::distance(std::begin(order),
-                      std::find(std::begin(order), std::end(order), cur_val)) +
-        1;
-    if (current_index >= order.size()) {
-      current_index = 0;
-    }
+    cur_val = order[cur_val];
   }
-
-  std::rotate(std::begin(order),
-              std::find(std::begin(order), std::end(order), 1),
-              std::end(order));
   std::string ans_str = "";
-  for (const auto& ele : order) {
-    ans_str += std::to_string(ele);
+  size_t temp = 1;
+  for (int i = 0; i < n_cups - 1; ++i) {
+    temp = order[temp];
+    ans_str += std::to_string(temp);
   }
-  ans_str.erase(0, 1);
   std::cout << ans_str << '\n';
   return 0;
 }
