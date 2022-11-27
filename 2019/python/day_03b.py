302c219d424666583cfd87aa3a8b78083e1eb40f
setop <7794056+setop@users.noreply.github.com>
2022-11-27 12:39:18 +0100

    small refactor while keeping the idea
    - use set(List) instead of recompute the whole thing
    - use stdin instead of hard-coded file to ease process of different samples
    - drop main of main
    - get rid of "math" import
    - small bits of syntax

--- 2019/python/day_03b.py
+++ 2019/python/day_03b.py
@@ -1,12 +1,6 @@
-#!/usr/bin/python3
-
-import math
-import sys
-
-
 def GetWireCoordinatesList(input):
-    init_x = 0
-    init_y = 0
+    """ mark all position covered by a wire """
+    init_x = init_y = 0
     wire = list()
     for inst in input:
         if inst[0] == "R":
@@ -27,56 +21,17 @@ def GetWireCoordinatesList(input):
                 wire.append((init_x, init_y))
     return wire
 
+import sys
+input1 = sys.stdin.readline().strip("\n").split(",")
+input2 = sys.stdin.readline().strip("\n").split(",")
 
-def GetWireCoordinatesSet(input):
-    init_x = 0
-    init_y = 0
-    wire = set()
-    for inst in input:
-        if inst[0] == "R":
-            for inc in range(1, int(inst[1:]) + 1):
-                init_x += 1
-                wire.add((init_x, init_y))
-        elif inst[0] == "U":
-            for inc in range(1, int(inst[1:]) + 1):
-                init_y += 1
-                wire.add((init_x, init_y))
-        elif inst[0] == "L":
-            for inc in range(1, int(inst[1:]) + 1):
-                init_x -= 1
-                wire.add((init_x, init_y))
-        elif inst[0] == "D":
-            for inc in range(1, int(inst[1:]) + 1):
-                init_y -= 1
-                wire.add((init_x, init_y))
-    return wire
+listwire1 = GetWireCoordinatesList(input1)
+listwire2 = GetWireCoordinatesList(input2)
+setwire2 = set(listwire2)  # to speed lookup
 
-
-def main():
-    # Get input
-    file_name = "../input/day_03_input"
-    if len(sys.argv) > 1:
-        file_name = sys.argv[1]
-    file = open(file_name)
-    input1 = file.readline().strip("\n").split(",")
-    input2 = file.readline().strip("\n").split(",")
-
-    # Solve
-    listwire1 = GetWireCoordinatesList(input1)
-    listwire2 = GetWireCoordinatesList(input2)
-    setwire2 = GetWireCoordinatesSet(input2)
-
-    man_dist = math.inf
-    distance = math.inf
-    for dist1, coor in enumerate(listwire1):
-        if coor in setwire2:
-            distance = min(distance, dist1 + listwire2.index(coor))
-    distance = (
-        distance + 2
-    )  # the list does not account for the central port as a coordinate to remove an if statement from the find intersection step
-    print(distance)
-    return distance
-
-
-if __name__ == "__main__":
-    main()
+distance = 1E9
+for dist1, coor in enumerate(listwire1):
+    if coor in setwire2:
+        distance = min(distance, dist1 + listwire2.index(coor))  # List.index is costly but happends only when there is an intersection, only 40 times for my input
+distance += 2  # the two lists do not contain (0,0) to save an if statement in the find intersection step, must add
+print(distance)
