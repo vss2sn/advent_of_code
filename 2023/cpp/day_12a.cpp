04d89ffd35d0d791157e99b62af24dc23f59cd7e
vss2sn <28676655+vss2sn@users.noreply.github.com>
2023-12-15 00:22:46 -0800

    Update 2023 day 12 cpp | Refactor a and cleanup b

--- 2023/cpp/day_12a.cpp
+++ 2023/cpp/day_12a.cpp
@@ -6,62 +6,39 @@
 #include <string>
 #include <vector>
 
-struct Record {
-  std::string damaged;
-  std::vector<int> duplicated;
-  int n_spaces;
+// Updated to use the method implemented in day_12b.cpp
+
+struct Key {
+  int reconstructed_size;
+  int duplicated_idxdx;
+
+  Key(const int reconstructed_size,
+      const int duplicated_idxdx) 
+      : reconstructed_size(reconstructed_size), duplicated_idxdx(duplicated_idxdx) {}
+
+  bool operator == (const Key& k) const {
+    return reconstructed_size == k.reconstructed_size && duplicated_idxdx == k.duplicated_idxdx;
+  }
 };
 
-bool compare(const std::string& damaged, const std::string& from_duplicate) {
-  for (int i = 0; i < from_duplicate.size(); i++) {
-    if (damaged[i] == '?') continue;
-    else if (from_duplicate[i] != damaged[i]) {
-      // std::cout << "false" << '\n';
+struct hasher {
+  std::size_t operator () (const Key& p) const {
+    return p.reconstructed_size * p.duplicated_idxdx;
+  }
+};
+
+struct Record {
+  std::string damaged; // damaged record
+  std::vector<int> duplicated; // duplicated record
+};
+
+bool compare(const std::string& damaged, const std::vector<char>& reconstructed) {
+  for (int i = 0; i < reconstructed.size(); i++) {
+    if (damaged[i] != '?' && reconstructed[i] != damaged[i]) {
       return false;
     }
   }
-  // std::cout << "true" << '\n';
   return true;
-}
-
-void permutation(const int depth, const Record& record, const std::string& from_duplicate, std::size_t& count, int spaces_used) {
-  // std::cout << "depth: " << depth << '\n';
-  // std::cout << "from_duplicate: " << from_duplicate << '\n';
-  if (!compare(record.damaged.substr(0, from_duplicate.size()), from_duplicate)) {
-    return;
-  }
-  if (depth == record.duplicated.size()-1) {
-    std::string temp = from_duplicate;
-    temp.append(record.damaged.size() - from_duplicate.size(), '.');
-    // std::cout << "Comparing full: " << temp << '\n';
-    count += compare(record.damaged, temp);
-    return;
-  }
-  for (int n_spaces = 1; n_spaces <= (record.n_spaces - spaces_used); n_spaces++) {
-    std::string new_from_duplicate = from_duplicate;
-    new_from_duplicate.append(n_spaces, '.');
-    new_from_duplicate.append(record.duplicated[depth+1], '#');
-    permutation(depth + 1, record, new_from_duplicate, count, spaces_used + n_spaces);
-  }
-}
-
-void permutation_main(const Record& record, std::size_t& count_main) {
-  int min_size_from_duplicates = record.duplicated.size() - 1; // min number of empty space
-  for (const auto & n : record.duplicated) {
-    min_size_from_duplicates += n; // spaces occupied by '#'
-  }
-  // std::cout << record.damaged.size() << ' ' << min_size_from_duplicates << '\n';
-  // exit(0);
-  std::size_t count = 0;
-  for (int i = 0; i <= record.damaged.size() - min_size_from_duplicates; i++) {
-    std::string from_duplicate = "";
-    from_duplicate.append(i, '.');
-    from_duplicate.append(record.duplicated[0], '#');
-    // std::cout << i << ' ' << record.damaged.size() << ' ' << min_size_from_duplicates << ' ' << from_duplicate << '\n';
-    permutation(0, record, from_duplicate, count, i);
-  }
-  // std::cout << count << '\n';
-  count_main += count;
 }
 
 std::vector<int> extract_numbers(const std::string& s) {
@@ -78,16 +55,100 @@ std::vector<int> extract_numbers(const std::string& s) {
   return numbers;
 }
 
+// Note that the last spring in reconstructed is always a '.'
+bool compare_with_duplicated(const std::vector<char>& reconstructed, const std::vector<int>& duplicated) {
+  int idx_duplicated = 0;
+  int idx_reconstructed = 0;
+  while (idx_reconstructed < reconstructed.size()) {
+    if (reconstructed[idx_reconstructed] == '#') {
+      for (int j = 0; j < duplicated[idx_duplicated]; j++) {
+        if (reconstructed[idx_reconstructed] != '#') return false;
+        idx_reconstructed++;
+      }
+      idx_duplicated++;
+      if (reconstructed[idx_reconstructed] != '.') return false;
+    }
+    idx_reconstructed++;
+  }
+  return true;
+}
+
+
+void dp(const std::string& damaged, std::vector<char>& reconstructed, const std::vector<int>& duplicated, std::size_t& count, const int duplicated_idx, std::unordered_map<Key, std::size_t, hasher>& seen) {
+  // Does not match pattern in damaged record (note compare matches upto the length of reconstructed only)
+  if (!compare(damaged, reconstructed)) {
+    return;
+  }
+
+  // Reconstructed pattern does not duplicate record
+  if (!compare_with_duplicated(reconstructed, duplicated)) {
+    return ;
+  }
+
+  // If the reconstructed record and the damage record ahve the same size and are equal
+  if (damaged.size() == reconstructed.size() && compare(damaged, reconstructed)) {
+    // If the number of damaged groups is correct
+    if (duplicated_idx == duplicated.size()) {
+      count++;
+    }
+    return;
+  }
+
+  // number of damaged + 1 * (number of intervals between each damaged group) > number of springs remaining 
+  // 1 is the minimm interval between each damaged group
+  if (std::begin(duplicated) + duplicated_idx <= std::end(duplicated) && ((std::accumulate(std::begin(duplicated) + duplicated_idx, std::end(duplicated), 0) + duplicated.size() - duplicated_idx) > (damaged.size() - reconstructed.size())) ) {
+    return;
+  }
+  
+  const std::size_t count_on_entry = count; // count on entry into dp function
+  // Util function for checking cache before calling the dp function
+  const auto& dp_wrapper_with_cache_check = [&damaged, &reconstructed, &duplicated, &count, &seen] (const int duplicated_idx) {
+    if (const auto it = seen.find(Key(reconstructed.size(), duplicated_idx)); it != seen.end()) {
+        count += it->second;
+      } else  {
+        dp(damaged, reconstructed, duplicated, count, duplicated_idx, seen);
+      }    
+  };
+  const int idx = reconstructed.size();
+  if (damaged[idx] == '?') { // '?' at idx
+    // Assume the '?' == '.'
+    { 
+      reconstructed.push_back('.'); 
+      dp_wrapper_with_cache_check(duplicated_idx);
+      reconstructed.pop_back();
+    }
+    // Assume the '?' == '#'
+    {
+      if (duplicated_idx < duplicated.size()) {
+        for(int j = 0; j < duplicated[duplicated_idx]; j++) {
+          reconstructed.push_back('#');
+        }
+        reconstructed.push_back('.'); // There is always a '.' after a group of damaged springs
+        dp_wrapper_with_cache_check(duplicated_idx+1);
+      }
+    }
+  } else if (damaged[idx] == '.') { // '.' at idx
+    reconstructed.push_back(damaged[idx]);
+    dp_wrapper_with_cache_check(duplicated_idx);
+  } else if (duplicated_idx < duplicated.size()) { // '#' at idx, bit for it to be a valid pattern the number of groups of '#' based on the duplicated record must not have all been seen
+    for(int j = 0; j < duplicated[duplicated_idx]; j++) {
+      reconstructed.push_back('#');
+    }
+    reconstructed.push_back('.'); // There is always a '.' after a group of damaged springs
+    dp_wrapper_with_cache_check(duplicated_idx+1);
+  }
+  while(reconstructed.size() > idx) { // revert to original state
+    reconstructed.pop_back();
+  }
+  seen[Key(reconstructed.size(), duplicated_idx)] = count - count_on_entry; // Update cache
+  return;
+}
+
 Record parse(const std::string& line) {
   const auto separator = line.find(' ');
   Record record;
   record.damaged = line.substr(0, separator);
-  // std::cout << record.damaged << " --> ";
   record.duplicated = extract_numbers(line.substr(separator + 1, line.size() - separator - 1));
-  // for (const auto & ele : record.duplicated) {
-  //   std::cout << ele << ' ';
-  // }
-  // std::cout << '\n';
   return record;
 }
 
@@ -102,20 +163,23 @@ int main(int argc, char * argv[]) {
   std::vector<Record> records;
   while(std::getline(file, line)) {
     // std::cout << line << '\n';
-    records.emplace_back(parse(line));
-  }
-  for (auto& record : records) {
-    int total = 0;
-    for (const auto ele : record.duplicated) {
-      total += ele;
-    }
-    record.n_spaces = record.damaged.size() - total;
+    const auto folded_record = parse(line);
+    Record unfolded_record;
+    unfolded_record.damaged.append(folded_record.damaged);
+    unfolded_record.damaged.push_back('.'); // Add a '.' to ensure that the last value in the input is a '.'
+    unfolded_record.duplicated.insert(std::end(unfolded_record.duplicated), std::begin(folded_record.duplicated), std::end(folded_record.duplicated));
+    records.emplace_back(unfolded_record);
   }
 
-  std::size_t count = 0;
+  std::size_t total = 0;
   for (const auto& record : records) {
-    permutation_main(record, count);
+    std::size_t count = 0;
+    std::vector<char> reconstructed;
+    std::unordered_map<Key, std::size_t, hasher> seen;
+    dp(record.damaged, reconstructed, record.duplicated, count, 0, seen);
+    total += count;
   }
-  std::cout << count << '\n';
+  std::cout << total << '\n';
+
   return 0;
 }
--- 2023/cpp/day_12b.cpp
+++ 2023/cpp/day_12b.cpp
@@ -8,26 +8,26 @@
 
 struct Key {
   int reconstructed_size;
-  int duplicated_idx;
+  int duplicated_idxdx;
 
   Key(const int reconstructed_size,
-      const int duplicated_idx) 
-      : reconstructed_size(reconstructed_size), duplicated_idx(duplicated_idx) {}
+      const int duplicated_idxdx) 
+      : reconstructed_size(reconstructed_size), duplicated_idxdx(duplicated_idxdx) {}
 
   bool operator == (const Key& k) const {
-    return reconstructed_size == k.reconstructed_size && duplicated_idx == k.duplicated_idx;
+    return reconstructed_size == k.reconstructed_size && duplicated_idxdx == k.duplicated_idxdx;
   }
 };
 
 struct hasher {
   std::size_t operator () (const Key& p) const {
-    return p.reconstructed_size * p.duplicated_idx;
+    return p.reconstructed_size * p.duplicated_idxdx;
   }
 };
 
 struct Record {
-  std::string damaged;
-  std::vector<int> duplicated;
+  std::string damaged; // damaged record
+  std::vector<int> duplicated; // duplicated record
 };
 
 bool compare(const std::string& damaged, const std::vector<char>& reconstructed) {
@@ -53,12 +53,7 @@ std::vector<int> extract_numbers(const std::string& s) {
   return numbers;
 }
 
-void print(const std::vector<char>& v) {
-  for (const auto ele : v) {
-    std::cout << ele;
-  }
-}
-
+// Note that the last spring in reconstructed is always a '.'
 bool compare_with_duplicated(const std::vector<char>& reconstructed, const std::vector<int>& duplicated) {
   int idx_duplicated = 0;
   int idx_reconstructed = 0;
@@ -76,126 +71,74 @@ bool compare_with_duplicated(const std::vector<char>& reconstructed, const std::
   return true;
 }
 
-void dp(const std::string& damaged, std::vector<char>& reconstructed, const std::vector<int>& duplicated, std::size_t& count, const int duplicated_i, std::unordered_map<Key, std::size_t, hasher>& seen) {
-  if ( const auto it = seen.find(Key(reconstructed.size(), duplicated_i)); it != seen.end()) {
-    count += it->second;
-    return;
-  }
 
-  const std::size_t count_on_entry_main = count;
-  // std::cout << "reconstructed: ";
-  // print(reconstructed);
-  // std::cout << '\n';
-  
-  if (duplicated_i > duplicated.size() + 1) {
-    // std::cout << duplicated_i << ' '<<  duplicated.size() + 1 << '\n';
-    // std::cout << "Number of damaged sets exceeds number in records" << '\n';
-    return;
-  }
-  
+void dp(const std::string& damaged, std::vector<char>& reconstructed, const std::vector<int>& duplicated, std::size_t& count, const int duplicated_idx, std::unordered_map<Key, std::size_t, hasher>& seen) {
+  // Does not match pattern in damaged record (note compare matches upto the length of reconstructed only)
   if (!compare(damaged, reconstructed)) {
-    // std::cout << "Comparison with damaged failed" << '\n';
-    // std::cout << damaged << '\n';
-    // print(reconstructed);
-    // std::cout << '\n';
     return;
   }
 
-    if (!compare_with_duplicated(reconstructed, duplicated)) {
-      // std::cout << "Comparison with duplicated failed" << '\n';
-      return ;
-    }
+  // Reconstructed pattern does not duplicate record
+  if (!compare_with_duplicated(reconstructed, duplicated)) {
+    return ;
+  }
 
+  // If the reconstructed record and the damage record ahve the same size and are equal
   if (damaged.size() == reconstructed.size() && compare(damaged, reconstructed)) {
-    // std::cout << "Size match" << '\n';
-    if (duplicated_i == duplicated.size()) {
-      // std::cout << damaged << " | ";
-      // print(reconstructed);
-      // std::cout << '\n';
+    // If the number of damaged groups is correct
+    if (duplicated_idx == duplicated.size()) {
       count++;
     }
-    // std::cout << "Size of reconstructed == size of damaged record" << '\n';
     return;
   }
 
-  if (std::begin(duplicated) + duplicated_i <= std::end(duplicated) && ((std::accumulate(std::begin(duplicated) + duplicated_i, std::end(duplicated), 0) + duplicated.size() - duplicated_i) > (damaged.size() - reconstructed.size())) ) {
-    // std::cout << "Not possible as number of damaged + 1 interval between each damaged set > number remaining" << '\n';
+  // number of damaged + 1 * (number of intervals between each damaged group) > number of springs remaining 
+  // 1 is the minimm interval between each damaged group
+  if (std::begin(duplicated) + duplicated_idx <= std::end(duplicated) && ((std::accumulate(std::begin(duplicated) + duplicated_idx, std::end(duplicated), 0) + duplicated.size() - duplicated_idx) > (damaged.size() - reconstructed.size())) ) {
     return;
   }
-
-  int idx = reconstructed.size();
-  const int idx_init = reconstructed.size();
-  if (damaged[idx] == '?') {
-    {
-      reconstructed.push_back('.'); 
-      if (const auto it = seen.find(Key(reconstructed.size(), duplicated_i)); it != seen.end()) {
-        // print(reconstructed);
-        // std::cout << '\n';
-        // std::cout << __LINE__ << " Seen before" << reconstructed.size() << ' ' << duplicated_i << '\n';
+  
+  const std::size_t count_on_entry = count; // count on entry into dp function
+  // Util function for checking cache before calling the dp function
+  const auto& dp_wrapper_with_cache_check = [&damaged, &reconstructed, &duplicated, &count, &seen] (const int duplicated_idx) {
+    if (const auto it = seen.find(Key(reconstructed.size(), duplicated_idx)); it != seen.end()) {
         count += it->second;
       } else  {
-        dp(damaged, reconstructed, duplicated, count, duplicated_i, seen);
-        // print(reconstructed);
-        // std::cout << '\n';
-      }
+        dp(damaged, reconstructed, duplicated, count, duplicated_idx, seen);
+      }    
+  };
+  const int idx = reconstructed.size();
+  if (damaged[idx] == '?') { // '?' at idx
+    // Assume the '?' == '.'
+    { 
+      reconstructed.push_back('.'); 
+      dp_wrapper_with_cache_check(duplicated_idx);
       reconstructed.pop_back();
     }
-    
+    // Assume the '?' == '#'
     {
-      if (duplicated_i < duplicated.size()) {
-        for(int j = 0; j < duplicated[duplicated_i]; j++) {
+      if (duplicated_idx < duplicated.size()) {
+        for(int j = 0; j < duplicated[duplicated_idx]; j++) {
           reconstructed.push_back('#');
         }
-        reconstructed.push_back('.');
-        if (const auto it = seen.find(Key(reconstructed.size(), duplicated_i + 1)); it != seen.end()) {
-          // print(reconstructed);
-          // std::cout << '\n';
-          // std::cout << __LINE__ << " Seen before" << reconstructed.size() << ' ' << duplicated_i + 1 << '\n';
-          count += it->second;
-        } else {
-          dp(damaged, reconstructed, duplicated, count, duplicated_i+1, seen);
-          // print(reconstructed);
-          // std::cout << '\n';
-        }
+        reconstructed.push_back('.'); // There is always a '.' after a group of damaged springs
+        dp_wrapper_with_cache_check(duplicated_idx+1);
       }
     }
-  } else {
-    if (damaged[idx] == '.') {
-      const int count_on_entry = count;      
-      reconstructed.push_back(damaged[idx]);
-      if (const auto it = seen.find(Key(reconstructed.size(), duplicated_i)); it != seen.end()) {
-        // print(reconstructed);
-        // std::cout << '\n';
-        // std::cout << __LINE__ << " Seen before" << reconstructed.size() << ' ' << duplicated_i << '\n';
-        count += it->second;
-      } else {
-        dp(damaged, reconstructed, duplicated, count, duplicated_i, seen);
-        // print(reconstructed);
-        // std::cout << '\n';
-      }
-    } else if (duplicated_i < duplicated.size()) {
-      const int count_on_entry = count;      
-      for(int j = 0; j < duplicated[duplicated_i]; j++) {
-        reconstructed.push_back('#');
-      }
-      reconstructed.push_back('.');
-      if (const auto it = seen.find(Key(reconstructed.size(), duplicated_i+1)); it != seen.end()) {
-        // print(reconstructed);
-        // std::cout << '\n';
-        // std::cout << __LINE__ << " Seen before" << reconstructed.size() << ' ' << duplicated_i+1 << '\n';
-        count += it->second;
-      } else {
-        dp(damaged, reconstructed, duplicated, count, duplicated_i+1, seen);
-        // print(reconstructed);
-        // std::cout << '\n';
-      }
+  } else if (damaged[idx] == '.') { // '.' at idx
+    reconstructed.push_back(damaged[idx]);
+    dp_wrapper_with_cache_check(duplicated_idx);
+  } else if (duplicated_idx < duplicated.size()) { // '#' at idx, bit for it to be a valid pattern the number of groups of '#' based on the duplicated record must not have all been seen
+    for(int j = 0; j < duplicated[duplicated_idx]; j++) {
+      reconstructed.push_back('#');
     }
+    reconstructed.push_back('.'); // There is always a '.' after a group of damaged springs
+    dp_wrapper_with_cache_check(duplicated_idx+1);
   }
-  while(reconstructed.size() > idx_init) {
+  while(reconstructed.size() > idx) { // revert to original state
     reconstructed.pop_back();
   }
-  // std::cout << __LINE__ << " Adding" << reconstructed.size() << ' ' << duplicated_i << ' ' << count - count_on_entry << '\n';
-  seen[Key(reconstructed.size(), duplicated_i)] = count - count_on_entry_main;
+  seen[Key(reconstructed.size(), duplicated_idx)] = count - count_on_entry; // Update cache
   return;
 }
 
@@ -203,12 +146,7 @@ Record parse(const std::string& line) {
   const auto separator = line.find(' ');
   Record record;
   record.damaged = line.substr(0, separator);
-  // std::cout << record.damaged << " --> ";
   record.duplicated = extract_numbers(line.substr(separator + 1, line.size() - separator - 1));
-  // for (const auto & ele : record.duplicated) {
-  //   std::cout << ele << ' ';
-  // }
-  // std::cout << '\n';
   return record;
 }
 
@@ -233,28 +171,16 @@ int main(int argc, char * argv[]) {
     unfolded_record.damaged.append(folded_record.damaged);
     unfolded_record.damaged.push_back('.'); // Add a '.' to ensure that the last value in the input is a '.'
     unfolded_record.duplicated.insert(std::end(unfolded_record.duplicated), std::begin(folded_record.duplicated), std::end(folded_record.duplicated));
-    // std::cout << unfolded_record.damaged << '\n';
-    // for (const auto ele : unfolded_record.duplicated) {
-    //   std::cout << ele << ' ' ;
-    // }
-    // std::cout << '\n';
     records.emplace_back(unfolded_record);
   }
 
   std::size_t total = 0;
   for (const auto& record : records) {
-    // std::cout << record.damaged << " | ";
-    // for (const auto & ele : record.duplicated) {
-    //   std::cout << ele << ' ';
-    // }
-    // std::cout << '\n';
     std::size_t count = 0;
     std::vector<char> reconstructed;
     std::unordered_map<Key, std::size_t, hasher> seen;
     dp(record.damaged, reconstructed, record.duplicated, count, 0, seen);
-    // std::cout << "Count: " << count << '\n';
     total += count;
-    // exit(0);
   }
   std::cout << total << '\n';
 
