f7027881b78fb3c28ed91fd86eae1b27662d3f08
vss2sn <28676655+vss2sn@users.noreply.github.com>
2024-12-06 00:27:49 -0800

    Add 2024 day 06 cpp

--- 2024/cpp/day_06b.cpp
+++ 2024/cpp/day_06b.cpp
@@ -17,7 +17,6 @@ struct Point {
     bool operator < (const Point& p) const {
         return p.row < row || (p.row == row && p.col < col);
     }
-
 };
 
 struct Hasher {
@@ -36,15 +35,14 @@ const std::vector<Point> moves {
 const bool in_map (const Point& p, const std::vector<std::string>& map) {
     return  !(p.col >= map[0].size() || p.col < 0 || p.row >= map.size() || p.row < 0);
 }
-std::unordered_set<Point, Hasher> visited;
+
 bool explore(Point current, 
              int idx,
-             std::vector<std::string> map, 
+             std::vector<std::string>& map, 
              std::unordered_map<Point, std::array<bool, 4>, Hasher> visited_edges, 
              std::unordered_set<Point, Hasher>& obs,
              const bool obstacle_placed) {
     while(in_map(current, map)) {
-        if (!obstacle_placed) visited.insert(current);
         if (!visited_edges.contains(current)) {
             visited_edges[current] = {{false, false, false, false}};    
         }
@@ -57,23 +55,15 @@ bool explore(Point current,
             idx += 1;
             idx %= 4;
         } else {
-            // if (!obstacle_placed) {
-            //     const auto c = map[next.row][next.col];
-            //     map[next.row][next.col] = '#'; 
-            //     auto next_idx = (idx+1)%4;
-            //     if (explore(current, next_idx, map, visited_edges, obs, true)) {
-            //         map[next.row][next.col] = 'O';
-            //         for (const auto& line : map) {
-            //             std::cout << line << '\n';
-            //         }
-            //         std::cout << '\n';
-            //         obs.insert(next);
-            //     }
-            //     map[next.row][next.col] = c; 
-            // }
+            if (!obstacle_placed && !visited_edges.contains(next)) {
+                const auto c = map[next.row][next.col];
+                map[next.row][next.col] = '#'; 
+                if (explore(current, (idx+1)%4, map, visited_edges, obs, true)) {
+                    obs.insert(next);
+                }
+                map[next.row][next.col] = c; 
+            }
             visited_edges[current][idx] = true;
-            map[current.row][current.col] = 'X';
-            map[next.row][next.col] = '*';
             current = next;
         }
     }
@@ -91,7 +81,6 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> map;
     Point start;
     while(std::getline(file, line)) {
-        // std::cout << line << '\n';
         map.push_back(line);
         for (const auto [idx, c] : std::views::enumerate(line)) {
             if (c == '^') {
@@ -100,19 +89,9 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    Point current = start;
-    int idx = 2;
     std::unordered_map<Point, std::array<bool, 4>, Hasher> visited_edges;
     std::unordered_set<Point, Hasher> obs;
-    explore(current, idx, map, visited_edges, obs, false);
-    int ans = 0;
-    for (const auto& v : visited) {
-        map[v.row][v.col] = '#';
-        if(explore(current, idx, map, visited_edges, obs, true)) ans++;
-        map[v.row][v.col] = '.';
-    }
+    explore(start, 2, map, visited_edges, obs, false);
     std::cout << obs.size() << '\n';
-    std::cout << visited.size() << '\n';
-    std::cout << ans << '\n';
     return 0;
-}
+} 
